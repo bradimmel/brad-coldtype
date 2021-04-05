2fd55e08fedf586887edc61c32df2488f880bbe9
bradimmel <bradimmel17@gmail.com>
2021-04-05 13:17:10 -0700

    stars change length with audio.amp; dead spot in center

--- animations/spaceflight.py
+++ animations/spaceflight.py
@@ -7,7 +7,7 @@ import random as pyRandom
 import math
 
 myFont = Font("assets/Secuela-Italic-v_1_787-TTF-VF.ttf")
-drums = MidiReader("demos/pz2/pz2_MIDI_new.mid", duration=900, bpm=82, fps=30)[0]
+drums = MidiReader("demos/pz2/pz2_MIDI_new.mid", duration=100, bpm=82, fps=30)[0]
 # wav playing still doesn't work yet
 wav = __sibling__("../demos/pz2/pz2_amp.wav")
 audio = Wavfile("demos/pz2/pz2_amp.wav")
@@ -17,20 +17,28 @@ wSize = 1500
 
 @animation((wSize,wSize), duration=drums.duration, audio=wav)
 def spaceflight(f):
-    kick = drums.fv(f.i, [36], [5, 50])
+    # kick = drums.fv(f.i, [36], [5, 50])
+    
+    amp = audio.amp(f.i)
 
     starSize = 5
     starCount = 100
+    starWarpSpeedSize = amp*100
     focalPoint = (f.a.r.mxx/2,f.a.r.mxy/2)
-    print(f.a.r)
+    deadSpotRad = 200
     stars = DATPens()
     for i in range(starCount):
-        starRect = Rect([pyRandom.randint(0,f.a.r.mxy-starSize), pyRandom.randint(0,f.a.r.mxx-starSize), starSize, starSize+20])
+        starRect = Rect([pyRandom.randint(0,f.a.r.mxy-starSize), pyRandom.randint(0,f.a.r.mxx-starSize), starSize, starSize+starWarpSpeedSize])
+        # todo: replace box w circle
+        if (starRect.x-focalPoint[0]) == 0 or (-deadSpotRad < (starRect.x-focalPoint[0]) < deadSpotRad and -deadSpotRad < (starRect.y-focalPoint[1]) < deadSpotRad):
+            continue
+        starOrient = math.degrees(math.atan((starRect.y-focalPoint[1])/(starRect.x-focalPoint[0])))-90
+
         stars.append(
             DATPen()
             .rect(starRect)
             .f(hsl(1,1,1))
-            .rotate(math.degrees(math.atan((starRect.y-focalPoint[1])/(starRect.x-focalPoint[0])))-90)
+            .rotate(starOrient)
         )
 
 
